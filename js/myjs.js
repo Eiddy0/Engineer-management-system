e16f17da0fa5d6e632be68747903a02e986426f7
Eiddy0 <921040168@qq.com>
2020-02-26 20:24:51 +0800

    增删改查

--- js/myjs.js
+++ js/myjs.js
@@ -1,44 +1,55 @@
 $(function(){
-	load();
 	$("#input").on("click",function(event){
-		$("#AddInf").show();
-		$("#FindInf").hide();
-		$("#DeleteInf").hide();
-		$("#ShowInf").hide();
+		$("#AddInf>form").show();
+		$("#FindInf>form").hide();
+		$("#DeleteInf>form").hide();
+		$("#ChangeInf>form").hide();
 	});
 	$("#find").on("click",function(event){
-		$("#AddInf").hide();
-		$("#FindInf").show();
-		$("#DeleteInf").hide();
-		$("#ShowInf").hide();
+		$("#AddInf>form").hide();
+		$("#FindInf>form").fadeIn(100);
+		$("#DeleteInf>form").hide();
+		$("#ChangeInf>form").hide();
 	});
 	$("#delete").on("click",function(event){
-		$("#AddInf").hide();
-		$("#FindInf").hide();
-		$("#DeleteInf").show();
-		$("#ShowInf").hide();
+		$("#AddInf>form").hide();
+		$("#FindInf>form").hide();
+		$("#DeleteInf>form").fadeIn(100);
+		$("#ChangeInf>form").hide();
 	});
-	$("#allInf").on("click",function(event){
-		$("#AddInf").hide();
-		$("#FindInf").hide();
-		$("#DeleteInf").hide();
-		$("#ShowInf").show();
+	$("#change").on("click",function(event){
+		$("#AddInf>form").hide();
+		$("#FindInf>form").hide();
+		$("#DeleteInf>form").hide();
+		$("#ChangeInf>.chg").fadeIn(100);
 	});
-	 
-	$("#SubInf").on("click",function(event){
+
+
+	$(".btn-close").on("click",function(event){
+		$(".btn-close").parents("form").hide();
+	});
+	
+	$("#btn-add").on("click",function(event){
 		var local=getDate();
 		local.push({
-			num:$("#num").val(),
-			name:$("#name").val(),
-			tel:$("#tel").val(),
-			age:$("#age").val(),
-			wage:$("#wage").val(),
-			sex:$("#sex").val(),//*********bug********
-			birth:$("#birth").val(),
-			edu:$("#edu").val(),
+			Rnum:$("#AddInf .Rnum").val(),
+			Rname:$("#AddInf .Rname").val(),
+			Rtel:$("#AddInf .Rtel").val(),
+			Rage:$("#AddInf .Rage").val(),
+			Rwage:$("#AddInf .Rwage").val(),
+			Rsex:$("#AddInf .Rsex").val(),//*********bug********
+			Rbirth:$("#AddInf .Rbirth").val(),
+			Redu:$("#AddInf .Redu").val(),
 		});
 		saveDate(local);
-		load();
+		// $("#AddInf .Rnum").val()='';
+		// $("#AddInf .Rname").val()='';
+		// $("#AddInf .Rtel").val()='';
+		// $("#AddInf .Rage").val()='';
+		// $("#AddInf .Rwage").val()='';
+		// $("#AddInf .Rsex").val()='';//*********bug********
+		// $("#AddInf .Rbirth").val()='';
+		// $("#AddInf .Redu").val()='';
 	});
 
 	function getDate() {
@@ -52,14 +63,20 @@ $(function(){
 	function saveDate(data){
 		localStorage.setItem("infor",JSON.stringify(data));
 	}
-	function load(){
-		var data=getDate();
-		$("#ShowInf").empty();
-		$.each(data,function(i,n) {
-			$("#ShowInf").prepend("<div>编号："+n.num+"</br>姓名："+n.name+"</br>电话："+n.tel+"</br>年龄："+n.age+"</br>基本工资："+n.wage+"</br>性别："+n.sex+"</br>生日："+n.birth+"</br>学历："+n.edu+"</br></br></div>");
+	function load(dt){
+		//var dt=getDate();
+		$("#InformCont").empty();
+		$.each(dt,function(i,n) {
+			$("#InformCont").prepend("<div>编号："+n.Rnum+"</br>姓名："+n.Rname+"</br>电话："+n.Rtel+"</br>年龄："+n.Rage+"</br>基本工资："+n.Rwage+"</br>性别："+n.Rsex+"</br>生日："+n.Rbirth+"</br>学历："+n.Redu+"</br></br></div>");
 		})
 	}
-	
+	$("#allInf").on("click",function(event){
+		$("#AddInf>form").hide();
+		$("#FindInf>form").hide();
+		$("#DeleteInf>form").hide();
+		var local=getDate();
+		load(local);
+	});
 	$(".ch1").on("click",function(event){
 		$(".iNum").show();
 		$(".iName").hide();
@@ -71,19 +88,68 @@ $(function(){
 	$("#btn-find").on("click",function(event){
 		var data = getDate();
 		var flag=0;
-		$(".infcontent").empty();
+		$("#InformCont").empty();
 		$.each(data,function(i,n) {
-			if(n.num == $("#FindInf .inputNum").val()||n.name == $("#FindInf .inputName").val()){
-				$(".infcontent").prepend("<div>编号："+n.num+"</br>姓名："+n.name+"</br>电话："+n.tel+"</br>年龄："+n.age+"</br>基本工资："+n.wage+"</br>性别："+n.sex+"</br>生日："+n.birth+"</br>学历："+n.edu+"</br></br></div>");
-			    flag=1;
+			if(n.Rnum == $("#FindInf .inputNum").val()||n.Rname == $("#FindInf .inputName").val()){
+				$("#InformCont").prepend("<div>编号："+n.Rnum+"</br>姓名："+n.Rname+"</br>电话："+n.Rtel+"</br>年龄："+n.Rage+"</br>基本工资："+n.Rwage+"</br>性别："+n.Rsex+"</br>生日："+n.Rbirth+"</br>学历："+n.Redu+"</br></br></div>");
+			    flag=1;return false;
 			}
 		})
-		if(!flag){$(".infcontent").prepend("没有符合要求的工程师信息！");}
-		
+		if(!flag){$("#InformCont").prepend("没有符合要求的工程师信息！");}
 	});
 	
-	$(".btn-close").on("click",function(event){
-		$(".btn-close").parents("form").hide();
+	$("#btn-delete").on("click",function(event){
+		var dt = getDate();
+		var flag=0;
+		$("#InformCont").empty();
+		$.each(dt,function(i,n) {
+			if(n.Rnum == $("#DeleteInf .inputNum").val()||n.Rname == $("#DeleteInf .inputName").val()){
+				dt.splice(i,1);flag=1;return false;
+			}
+		})
+		if(!flag){$("#InformCont").prepend("没有符合要求的工程师信息！");}
+		else {$("#InformCont").prepend("已删除该工程师信息！");}
+		saveDate(dt);
 	});
-	
+
+	$("#btn-change1").on("click",function(event){
+		var dt = getDate();
+		var flag1=0;
+		$("#InformCont").empty();
+		$.each(dt,function(i,n) {
+			if(n.Rnum == $("#ChangeInf .inputNum").val()||n.Rname == $("#ChangeInf .inputName").val()){
+				$("#change-form").fadeIn(100);
+				$("#change-form .Rnum").val(n.Rnum);
+				$("#change-form .Rname").val(n.Rname);
+				$("#change-form .Rtel").val(n.Rtel);
+				$("#change-form .Rage").val(n.Rage);
+				$("#change-form .Rwage").val(n.Rwage);
+				$("#change-form .Rsex").val(n.Rsex);//*********bug*******
+				$("#change-form .Rbirth").val(n.Rbirth);
+				$("#change-form .Redu").val(n.Redu);
+				$("#btn-change2").on("click",function(event){
+					n.Rnum=$("#change-form .Rnum").val();
+					n.Rname=$("#change-form .Rname").val();
+					n.Rtel=$("#change-form .Rtel").val();
+					n.Rage=$("#change-form .Rage").val();
+					n.Rwage=$("#change-form .Rwage").val();
+					n.Rsex=$("#change-form .Rsex").val();//*********bug*******
+					n.Rbirth=$("#change-form .Rbirth").val();
+					n.Redu=$("#change-form .Redu").val();
+					saveDate(dt);
+					$("#InformCont").prepend("已修改该工程师信息！");
+					$("#btn-change2").parents("#change-form").hide();
+				});
+				$("#change-form .btn-close").on("click",function(event){
+					$("#InformCont").prepend("修改工程师信息失败！");
+				});
+				//**************打印提示信息bug************
+				flag1=1;
+				return false;
+			}
+		})
+		if(!flag1){$("#InformCont").prepend("没有符合要求的工程师信息！");}
+	});
+
+
 })
